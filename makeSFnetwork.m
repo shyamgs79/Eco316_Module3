b58b736745e16fdcf165314df97b1ec133b8aa64
shyamgs79 <124452016+shyamgs79@users.noreply.github.com>
2023-10-19 12:52:35 -0400

    Add files via upload

--- makeSFnetwork.m
+++ makeSFnetwork.m
@@ -5,23 +5,32 @@ clear all
 tic
 %number of agents
 n=500;
-totalpossconnections=n*(n-1);
+totalpossconnections=(n*(n-1))/2;
 
 %number of connections desired is c(note n << c << totalpossconnections)
-c=1500;
+c=1000;
 
 %strength of preference (typically, p >= 1)
-p=1;
+p=2;
 
-%SF=zeros(n);
+SF=zeros(n);
 %SF=sparse(zeros(n));
 %SF=false(n);
-SF=sparse(false(n));
+%SF=sparse(false(n));
 
 
 for i=1:n
-    x=randi(n);
-    SF(i,x)=1;
+    
+    diffn=0;
+    while diffn==0
+        x=randi(n);
+        if x~=i
+            SF(i,x)=1;
+            SF(x,i)=1;
+            diffn=1;
+        end
+    end
+    
 end
 
 
@@ -44,17 +53,25 @@ while connectionsmade<c
     
     x=randi(n);
     
-    if SF(nodechosen,x)==0
+    if SF(nodechosen,x)==0 && nodechosen~=x
         SF(nodechosen,x)=1;
+        SF(x,nodechosen)=1;
         connectionsmade=connectionsmade+1;
     end
 end
 
 degrees=sum(SF,2);
-
+figure(1)
 plot(degrees);
+figure(2)
 histogram(degrees);
 toc
 
 [gclustering] = findclusteringcoeff(SF);
 B=findshortestpaths(SF);
+
+G=graph(SF);
+figure(3)
+plot(G);
+figure(4)
+spy(SF);
